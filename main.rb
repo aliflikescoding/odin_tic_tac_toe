756b574ccd02d6638de825a44e8fb48c2f273fff
Alif Wibowo <alif.likes.coding@gmail.com>
2023-06-16 07:01:44 +0700

    fixed newline when getting input

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 puts 'plese input the name of the first user'
-name_user1 = gets
+name_user1 = gets.chomp
 puts 'plese input the name of the second user' 
-name_user2 = gets
+name_user2 = gets.chomp
