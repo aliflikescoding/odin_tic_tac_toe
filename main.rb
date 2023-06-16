7057f530b0da1a011c8092013b3c6433f2197f65
Alif Wibowo <alif.likes.coding@gmail.com>
2023-06-16 09:57:35 +0700

    Add user class

--- main.rb
+++ main.rb
@@ -2,3 +2,18 @@ puts 'plese input the name of the first user'
 name_user1 = gets.chomp
 puts 'plese input the name of the second user' 
 name_user2 = gets.chomp
+
+# the user class
+class User
+  def initialize(name)
+    @name = name
+    @pick = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]]
+  end
+
+  def to_s
+    puts "user class with the name of #{@name}"
+  end
+
+end
